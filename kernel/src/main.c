5689e7581b4c72df7fcf18834b1ff5fb0f80ce9a
Sun <astrolithium@gmail.com>
2025-03-31 22:15:07 +0800

    add hello world

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,3 +1,5 @@
+#include <flanterm/flanterm.h>
+#include <flanterm/backends/fb.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
@@ -112,11 +114,31 @@ void kmain(void) {
     // Fetch the first framebuffer.
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
+    struct flanterm_context *ft_ctx = flanterm_fb_init(
+        NULL,
+        NULL,
+        framebuffer->address, framebuffer->width, framebuffer->height, framebuffer->pitch,
+        framebuffer->red_mask_size, framebuffer->red_mask_shift,
+        framebuffer->green_mask_size, framebuffer->green_mask_shift,
+        framebuffer->blue_mask_size, framebuffer->blue_mask_shift,
+        NULL,
+        NULL, NULL,
+        NULL, NULL,
+        NULL, NULL,
+        NULL, 0, 0, 1,
+        0, 0,
+        0
+    );
+
+    const char msg[] = "Hello world\n";
+
+    flanterm_write(ft_ctx, msg, sizeof(msg));
+
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
-    for (size_t i = 0; i < 100; i++) {
-        volatile uint32_t *fb_ptr = framebuffer->address;
-        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
-    }
+    // for (size_t i = 0; i < 100; i++) {
+    //     volatile uint32_t *fb_ptr = framebuffer->address;
+    //     fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    // }
 
     // We're done, just hang...
     hcf();
